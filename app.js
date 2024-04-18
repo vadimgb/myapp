a9e115f4223426b545831a949424643d05fa60e9
vadimgb <vadimgb@yandex.ru>
2024-04-18 11:28:45 +0700

    first commit

---
+++ app.js
@@ -0,0 +1,10 @@
+import express from "express"
+const app = express()
+const port = process.env.PORT || 8080
+app.get("/", (req, res) => {res.send("Hello, World!")})
+
+app.listen(port,
+    () => {
+        console.log(`App listen port: ${port}`)
+    }
+)
